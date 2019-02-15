152e9a288a02f66f5741625c806849b1e7eb75cb
Alex Kornitzer <alex.kornitzer@countercept.com>
2019-02-15 10:27:14 +0000

    config: use a config builder

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #![allow(non_snake_case)]
 #![allow(unused_doc_comments)]
 
-
 #[macro_use]
 extern crate error_chain;
 extern crate reqwest;
@@ -21,15 +20,15 @@ mod request;
 
 use std::time::Duration;
 
-use reqwest::ClientBuilder;
 use reqwest::Client as HttpClient;
+use reqwest::ClientBuilder;
 
 use errors::Result;
 use errors::ResultExt;
 
 #[derive(Clone, Debug)]
 pub struct Client {
-    config: Config,
+    pub config: Config,
 }
 
 impl Client {
@@ -39,30 +38,71 @@ impl Client {
 }
 
 #[derive(Clone, Debug)]
-pub struct Config {
+pub struct ConfigBuilder {
     pub address: String,
     pub datacenter: Option<String>,
-    pub http_client: HttpClient,
-    pub wait_time: Option<Duration>,
 }
 
-impl Config {
-    pub fn new() -> Result<Config> {
+impl ConfigBuilder {
+    pub fn new() -> ConfigBuilder {
+        ConfigBuilder {
+            address: String::from("http://localhost:8500"),
+            datacenter: None,
+        }
+    }
+
+    pub fn address<I>(mut self, url: I) -> ConfigBuilder
+    where
+        I: Into<String>,
+    {
+        self.address = url.into();
+        self
+    }
+
+    pub fn datacenter<I>(mut self, name: I) -> ConfigBuilder
+    where
+        I: Into<String>,
+    {
+        self.datacenter = Some(name.into());
+        self
+    }
+
+    pub fn build(self) -> Result<Config> {
         ClientBuilder::new()
             .build()
             .chain_err(|| "Failed to build reqwest client")
-            .map(|client| {
-                Config {
-                    address: String::from("http://localhost:8500"),
-                    datacenter: None,
-                    http_client: client,
-                    wait_time: None,
-                }
+            .map(|client| Config {
+                address: self.address,
+                datacenter: self.datacenter,
+                http_client: client,
             })
     }
 }
 
+#[derive(Clone, Debug)]
+pub struct Config {
+    address: String,
+    datacenter: Option<String>,
+    http_client: HttpClient,
+}
 
+impl Config {
+    pub fn builder() -> ConfigBuilder {
+        ConfigBuilder::new()
+    }
+
+    pub fn address(&self) -> &String {
+        &self.address
+    }
+
+    pub fn datacenter(&self) -> &Option<String> {
+        &self.datacenter
+    }
+
+    pub fn client(&self) -> &HttpClient {
+        &self.http_client
+    }
+}
 
 #[derive(Clone, Debug, Default)]
 pub struct QueryOptions {
@@ -70,8 +110,6 @@ pub struct QueryOptions {
     pub wait_index: Option<u64>,
     pub wait_time: Option<Duration>,
 }
-
-
 
 #[derive(Clone, Debug)]
 pub struct QueryMeta {
