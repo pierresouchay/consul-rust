0c1de215d8dbefe5932e28b8057130c027b95b12
Niel Drummond <niel.drummond@fundingcircle.com>
2019-11-19 12:21:35 +0000

    Add mockito test for optional kv

--- tests/kv.rs
+++ tests/kv.rs
@@ -1,6 +1,8 @@
 extern crate consul;
-use consul::kv::KVPair;
+use consul::kv::{KVPair, KV};
 use consul::{Client, Config};
+
+use mockito::mock;
 
 #[test]
 fn kv_test() {
@@ -25,4 +27,34 @@ fn kv_test() {
 
     let r = client.list("", None).unwrap();
     assert!(r.0.is_empty());
+}
+
+#[test]
+fn test_null_value() {
+    let _m = mock("GET", "/v1/kv/a-path/?recurse=")
+        .with_status(201)
+        .with_header("Content-Type", "application/json")
+        .with_body("[{\"LockIndex\":0,\"Key\":\"a-path/\",\"Flags\":0,\"Value\":null,\"CreateIndex\":84,\"ModifyIndex\":84}]")
+        .create();
+
+    let expected: Vec<KVPair> = vec![KVPair {
+        Key: String::from("a-path/"),
+        Value: None,
+        CreateIndex: Some(84),
+        ModifyIndex: Some(84),
+        LockIndex: Some(0),
+        Flags: Some(0),
+        ..Default::default()
+    }];
+
+    let mockito_address = mockito::server_url();
+
+    let mut config = consul::Config::new().unwrap();
+
+    config.address = mockito_address;
+
+    let client = consul::Client::new(config);
+    let res = client.list("a-path/", None);
+
+    assert_eq!(expected, res.unwrap().0);
 }
