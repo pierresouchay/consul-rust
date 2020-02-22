c75946f369fcdab8ff55aeb50dcf403ae957aeb2
Pierre Souchay <p.souchay@criteo.com>
2020-02-22 15:42:32 +0100

    [BUGFIX] Fix incorrect return values for services() in catalog

--- tests/catalog.rs
+++ tests/catalog.rs
@@ -1,5 +1,4 @@
 extern crate consul;
-use consul::catalog::Catalog;
 use consul::{Client, Config};
 
 #[test]
@@ -9,4 +8,17 @@ fn ds_test() {
     let client = Client::new(config);
     let r = client.datacenters().unwrap();
     assert_eq!(r.0, ["dc1"]);
+}
+
+#[test]
+fn ds_services_test() {
+    use consul::catalog::Catalog;
+    let config = Config::new().unwrap();
+    let client = Client::new(config);
+    let r = client.services(Option::None).unwrap();
+    assert_ne!(r.0.len(), 0);
+    match r.0.get("consul") {
+        None      => panic!("Should have a Consul service"),
+        Some(val) => assert_eq!(val.len(), 0) // consul has no tags
+    }
 }
