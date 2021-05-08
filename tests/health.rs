f1067cf422d3d86dfe9c17f1ab17721eeb3b42a6
Tomer Shalev <tshalev@proofpoint.com>
2021-05-08 22:02:01 +0300

    Fix various clippy issues
    Fixes some of the issues found by:
    $ cargo clippy --all-targets --all-features -- -D warnings

--- tests/health.rs
+++ tests/health.rs
@@ -12,7 +12,7 @@ fn health_test() {
         .unwrap();
     let (snodes, meta) = (r.0, r.1);
     {
-        assert!(snodes.len() > 0, "should have at least one Service Node");
+        assert!(!snodes.is_empty(), "should have at least one Service Node");
         assert!(meta.last_index.unwrap() > 0, "index must be positive");
     }
     // A non existing, should be empty
